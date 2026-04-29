9fd86c6db170c02200b5353aa2b77050bf78c9a5
7aclimb <7a.climb@gmail.com>
2026-04-29 22:32:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -69,7 +69,7 @@
 
   if (!countEl) return;
 
-  const WORKER_URL = "PEGA_AQUI_TU_URL_DEL_WORKER";
+  const WORKER_URL = https://verkami-7a-counter.laster3000.workers.dev/;
 
   async function loadFollowers() {
     try {
